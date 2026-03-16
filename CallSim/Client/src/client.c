b638dcf581c73c2cae63084b32191b01b05284a7
Tomas Homola <tomas.homola123@gmail.com>
2026-03-16 08:28:44 +0100

    feat: convert C to C++

--- CallSim/Client/src/client.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Client is running!\n");
-    return 0;
-}
-
--- CallSim/Server/src/server.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Server is running!\n");
-    return 0;
-}
-
