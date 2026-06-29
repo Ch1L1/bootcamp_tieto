bf2fde4362de69c21ab87c841385a9f349dd1670
Tomas Homola <xhomola1@fi.muni.cz>
2026-06-29 18:00:18 +0200

    feature: basic generated GTests for funcionality

--- CallSim/Client/test/ClientTests.cpp
+++ CallSim/Client/test/ClientTests.cpp
@@ -1,18 +1,32 @@
 #include <gtest/gtest.h>
 #include "ClientStateMachine.hpp"
 
-TEST(ClientFSM, InitialStateIsConnected) {
+TEST(ClientFSMTests, SuccessfulRegistration) {
     ClientStateMachine fsm;
     EXPECT_EQ(fsm.get_current_state(), callsim::CLIENT_CONNECTED);
-}
-
-TEST(ClientFSM, AllowedTransitionSucceeds) {
-    ClientStateMachine fsm;
-    EXPECT_NO_THROW(fsm.handle_transition(callsim::REGISTERED, callsim::CLIENT_REGISTERED));
+    
+    fsm.handle_transition(callsim::REGISTERED, callsim::CLIENT_REGISTERED);
     EXPECT_EQ(fsm.get_current_state(), callsim::CLIENT_REGISTERED);
 }
 
-TEST(ClientFSM, InvalidTransitionThrowsException) {
+TEST(ClientFSMTests, OutgoingCallSuccess) {
     ClientStateMachine fsm;
-    EXPECT_THROW(fsm.handle_transition(callsim::ANSWER, callsim::CLIENT_ANSWERING), std::runtime_error);
+    fsm.handle_transition(callsim::REGISTERED, callsim::CLIENT_REGISTERED);
+    
+    fsm.handle_transition(callsim::CALL, callsim::CLIENT_CALLING);
+    EXPECT_EQ(fsm.get_current_state(), callsim::CLIENT_CALLING);
+}
+
+TEST(ClientFSMTests, IncomingCallSuccess) {
+    ClientStateMachine fsm;
+    fsm.handle_transition(callsim::REGISTERED, callsim::CLIENT_REGISTERED);
+    
+    fsm.handle_transition(callsim::CALL, callsim::CLIENT_ANSWERING);
+    EXPECT_EQ(fsm.get_current_state(), callsim::CLIENT_ANSWERING);
+}
+
+TEST(ClientFSMTests, InvalidTransitionThrows) {
+    ClientStateMachine fsm;
+    
+    EXPECT_THROW(fsm.handle_transition(callsim::CALL, callsim::CLIENT_CALLING), std::runtime_error);
 }
