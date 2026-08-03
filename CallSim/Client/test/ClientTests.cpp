a6bc96c32c759288b1cf42567f63fcd3f3ec6457
Tomas Homola <tomas.homola123@gmail.com>
2026-08-03 14:42:20 +0200

    feat: more tests for both client and server

--- CallSim/Client/test/ClientTests.cpp
+++ CallSim/Client/test/ClientTests.cpp
@@ -36,3 +36,31 @@ TEST(ClientUIHelpersTests, CallConnectedMessageUsesConsistentFormat) {
     EXPECT_EQ(format_call_connected_message("Bob"), "[CALL CONNECTED] Now talking with Bob.");
     EXPECT_EQ(format_call_connected_message("Lora"), "[CALL CONNECTED] Now talking with Lora.");
 }
+
+TEST(ClientFSMTests, RejectedOutgoingCallReturnsToRegistered) {
+    ClientStateMachine fsm;
+    fsm.handle_transition(callsim::REGISTERED);
+    fsm.handle_transition(callsim::CALL);
+    fsm.handle_transition(callsim::REJECTED);
+
+    EXPECT_EQ(fsm.get_current_state(), callsim::CLIENT_REGISTERED);
+}
+
+TEST(ClientFSMTests, AcceptedIncomingCallMovesToTalking) {
+    ClientStateMachine fsm;
+    fsm.handle_transition(callsim::REGISTERED);
+    fsm.handle_transition(callsim::ANSWERING);
+    fsm.handle_transition(callsim::ACCEPTED);
+
+    EXPECT_EQ(fsm.get_current_state(), callsim::CLIENT_TALKING);
+}
+
+TEST(ClientFSMTests, TalkingStateCanEndTheCall) {
+    ClientStateMachine fsm;
+    fsm.handle_transition(callsim::REGISTERED);
+    fsm.handle_transition(callsim::ANSWERING);
+    fsm.handle_transition(callsim::ACCEPTED);
+    fsm.handle_transition(callsim::END);
+
+    EXPECT_EQ(fsm.get_current_state(), callsim::CLIENT_REGISTERED);
+}
